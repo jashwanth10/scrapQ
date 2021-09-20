bbf1dcf549298fd784e3235310d03f7ac21c8225
anudeepch6789 <anudeepch528@gmail.com>
2021-09-20 20:15:34 +0530

    comments added

--- orgChart/project/app/chart-org/chart-org.component.js
+++ orgChart/project/app/chart-org/chart-org.component.js
@@ -2,68 +2,67 @@
 
 var app = angular.module('chartOrg', []);
 var max_level = 1;
+
+// fetching the data
 fetch("data/org-tree-data.json")
     .then(function(resp){
         return resp.json();
     })
     .then(function(data){
+        // Travelling through the data. performing DFS and storing the data
         traverse(data[0],1);
     });
+
 
 var nodes = []
 var nodeDict = {}
 var location_map = []
 
+
 function setData(node){
-    // console.log(nodeDict[node]);
+
     location_map = [];
-    console.log("AJKSA", node)
     locationTraverse(nodeDict[node]);
+    // localStorage is used for data transfer to test.html(google map integration)
     localStorage["data"]  = JSON.stringify(location_map);
+
 }
 
+// collecting data for google map
 function locationTraverse(node){
+
     var temp ;
-    // console.log(node.latitude);
-    // console.log(node.longitude,"helllllllllllllllllllllllllllll");
-    // temp = [node.userName,node.latitude,node.longitude,node.img];
 
-
+    // temp = [node.userName,node.latitude,node.longitude,node.img,node.userMobile,node.userType];
     // for checking purpose, for usage uncomment above one
-
-    temp = [node.userName,Math.random()*100,Math.random()*100,"https://cdn.balkan.app/shared/3.jpg",node.userMobile,node.userType]
-    // console.log(temp);
+    temp = [node.userName,Math.random()*90,Math.random()*90,"https://cdn.balkan.app/shared/3.jpg",node.userMobile,node.userType]
     location_map.push(temp);
     if('children' in node){
         node.children.forEach(element => {
             locationTraverse(element);
         });
     }
+
 }
 
 
+// performing DFS
 function traverse(node,level){
 
     nodeDict[node.userId] = node;
 
     var temp;
-    // console.log(node);
-    
-    // for img node.img should be used if we have images
-    // console.log(temp);
     var immediate_children = 0;
     if('children' in node)
     {
-        // console.log(node.children.length,"hellooo");
         immediate_children = node.children.length;
-
     }
-    
     var count_children = 0;
-    
 
     if('children' in node){
+        
         node.children.forEach(element => {
+            // traversing through the children
             count_children = count_children+ traverse(element,level+1);
             if(level+1>max_level)
             {
@@ -72,6 +71,7 @@ function traverse(node,level){
         });
         
     }
+    // storing the needed information from json and performmed operations(replace img)
     temp = {
         "id": node.userId,"pid":node.ownerId, "name": node.userName,"tags":[(level).toString()],
         "work" : node.userType,
@@ -82,37 +82,17 @@ function traverse(node,level){
         "immediate_children" :"Immediate Reportees:"+immediate_children,
         "count_children" : "Total Reportees:"+count_children,
     };
-    // console.log(temp);
-    // console.log(temp);
     nodes.push(temp);
     return count_children+1;
     
 }
 
+// declaring the Orgchart Template and specifing Node structure
 OrgChart.templates.myTemplate = Object.assign({}, OrgChart.templates.ula);
 OrgChart.templates.myTemplate.node = '<circle cx="100" cy="100" r="100" fill="#4D4D4D" stroke-width="1" stroke="#1C1C1C"></circle>'; 
 OrgChart.templates.myTemplate.node ='<line x1="0" y1="0" x2="250" y2="0" stroke-width="20" stroke="#039BE5"></line>' +'<rect x="0" y="0" height="120" width="250" fill="#ffffff" stroke-width="1" stroke="#aeaeae"></rect>'
 
-
-OrgChart.templates.green = Object.assign({}, OrgChart.templates.ula);
-OrgChart.templates.green.node = '<line x1="0" y1="0" x2="250" y2="0" stroke-width="20" stroke="#000000"></line>' +'<rect x="0" y="0" height="120" width="250" fill="#ffffff" stroke-width="1" stroke="#aeaeae"></rect>'
-
-OrgChart.templates.red = Object.assign({}, OrgChart.templates.ula);
-OrgChart.templates.red.node = '<line x1="0" y1="0" x2="250" y2="0" stroke-width="20" stroke="red"></line>' +'<rect x="0" y="0" height="120" width="250" fill="#ffffff" stroke-width="1" stroke="#aeaeae"></rect>'
-
-
-OrgChart.templates.blue = Object.assign({}, OrgChart.templates.ula);
-OrgChart.templates.blue.node = '<line x1="0" y1="0" x2="250" y2="0" stroke-width="20" stroke="blue"></line>' +'<rect x="0" y="0" height="120" width="250" fill="#ffffff" stroke-width="1" stroke="#aeaeae"></rect>'
-
-
-OrgChart.templates.yellow = Object.assign({}, OrgChart.templates.ula);
-OrgChart.templates.yellow.node = '<line x1="0" y1="0" x2="250" y2="0" stroke-width="20" stroke="yellow"></line>' +'<rect x="0" y="0" height="120" width="250" fill="#ffffff" stroke-width="1" stroke="#aeaeae"></rect>'
-
-// var just_name = 
-// OrgChart.templates.node_1 = Object.assign({}, OrgChart.templates.ula);
-// OrgChart.templates.node_1.node = '<line x1="0" y1="0" x2="250" y2="0" stroke-width="20" stroke="pink"></line>' +'<rect x="0" y="0" height="120" width="250" fill="#ffffff" stroke-width="1" stroke="#aeaeae"></rect>'
-// OrgChart.templates.ula.field_3 = 
-// '<text class="field_3" style="font-size: 14px;" fill="#000000" x="120" y="100" text-anchor="right-bottom">{val}</text>';
+// for map icon, immediate reportee, total reportee placing them in the node
 OrgChart.templates.ula.field_6 = 
 '<text style="font-size: 13px;" fill="#000000" x="5" y="135" >{val}</text>';
 OrgChart.templates.ula.field_2 = 
@@ -122,22 +102,29 @@ OrgChart.templates.ula.field_7 =
 '<text style="font-size: 13px;border: 2px solid black;" fill="#000000" x="180" y="135" >{val}</text>';
  OrgChart.templates.ula.size = [300, 150];
 
-
+//Returns random color for each level
 function getRandomColor() {
+    
     var letters = '0123456789ABCDEF';
     var color = '#';
     for (var i = 0; i < 6; i++) {
       color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
-  }
+
+}
+
 var current = 0;
 var tags_check;
 tags_check = {};
+
+// toggle function to change the orientation
 function toggle_button(){
+   
     console.log(tags_check,"tags_check");
     if(current ==0 )
     {
+
         var chart = new OrgChart(document.getElementById("hello"), {
 
             mouseScrool: OrgChart.action.scroll,
@@ -175,8 +162,10 @@ function toggle_button(){
             nodes: nodes
         });
         current = 1;
+
     }
     else{
+
         var chart = new OrgChart(document.getElementById("hello"), {
 
             mouseScrool: OrgChart.action.scroll,
@@ -192,6 +181,7 @@ function toggle_button(){
             tags : tags_check,
             miniMap: true,
             nodeBinding: {
+
                 field_0: "name",
                 field_2:"work",
                 field_1:"mobile",
@@ -204,14 +194,17 @@ function toggle_button(){
                 
             },
             toolbar: {
+
                 fullScreen: true,
                 zoom: true,
                 fit: true,
                 expandAll: true
-              },
+              
+            },
             nodes: nodes
         });
         current = 0;
+        
     }
     
 }
@@ -221,20 +214,18 @@ angular.
     module('chartOrg').
     component('chartOrg', {
         templateUrl: 'chart-org/chart-org.template.html' ,
+        
         controller: function chartOrgController(){
-            // var tags_check;
-            // tags_check = {};
+
+            // color coding for all the levels, where max level is calculated in our traverse function
             for(var teee=1;teee<=max_level;teee++)
             {
+                
                 var name_string = "node"+'_'+teee.toString();
-                    // OrgChart.templates.name_string = Object.assign({}, OrgChart.templates.ula);
                 OrgChart.templates[name_string] = Object.assign({}, OrgChart.templates.ula);
                 var stroke_color = getRandomColor()
 
-                // OrgChart.templates[name_string].node = '<line x1="0" y1="0" x2="250" y2="0" stroke-width="20" stroke="pink"></line>' +'<rect x="0" y="0" height="120" width="250" fill="#ffffff" stroke-width="1" stroke="#aeaeae"></rect>'
                 OrgChart.templates[name_string].node = `<line x1="0" y1="0" x2="300" y2="0" stroke-width="20" stroke="${stroke_color}"></line>' +'<rect x="0" y="0" height="150" width="300" fill="#ffffff" stroke-width="1" stroke="#aeaeae"></rect>`
-                // console.log(stroke_color,"strokee colorrr");
-                // OrgChart.templates.name_string.node = '<line x1="0" y1="0" x2="250" y2="0" stroke-width="20" stroke="black"></line>' +'<rect x="0" y="0" height="120" width="250" fill="#ffffff" stroke-width="1" stroke="#aeaeae"></rect>'
                 var temp;
                 var curr_level = teee.toString();
                 temp = {template:name_string};
@@ -242,7 +233,7 @@ angular.
             
             }   
             
-           
+           // For Div id - hello Orgchart is rendered
             this.chart = new OrgChart(document.getElementById("hello"), {
 
                 mouseScrool: OrgChart.action.scroll,
@@ -256,9 +247,14 @@ angular.
 
                 template: "myTemplate",
                 
+                // tags check for color mapping etc
                 tags : tags_check,
+
+                // mini map 
                 miniMap: true,
+                //node binding, the things which we want to show in the node.
                 nodeBinding: {
+
                     field_0: "name",
                     field_2:"work",
                     field_1:"mobile",
@@ -272,21 +268,17 @@ angular.
                     
                 },
                 toolbar: {
+
                     fullScreen: true,
                     zoom: true,
                     fit: true,
                     expandAll: true
-                  },
+                  
+                },
                 nodes: nodes
             });
 
             
-
-            // console.log(this.chart._layoutConfigs["base"]["orientation"]);
-            // this.chart._layoutConfigs["base"]["orientation"] = 3;
-            // console.log(this.chart._layoutConfigs["base"]["orientation"]);
-            
-
 
             this.chart.on('field', function(sender, args){
                 console.log("checkkk");
